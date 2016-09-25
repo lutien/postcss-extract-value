d2cb78f8e51d703fba48be6aebcbe433bd407aef
lutien <lutien02@gmail.com>
2016-09-25 16:06:51 +0300

    Update devDependencies, add eslint-config-airbnb-base

--- test.js
+++ test.js
@@ -1,27 +1,27 @@
 import postcss from 'postcss';
-import test    from 'ava';
+import test from 'ava';
 
 import plugin from './';
 
 function run(t, input, output, opts = { }) {
-    return postcss([ plugin(opts) ]).process(input)
-        .then( result => {
+    return postcss([plugin(opts)]).process(input)
+        .then((result) => {
             t.deepEqual(result.css, output);
             t.deepEqual(result.warnings().length, 0);
         });
 }
 
 const optDefault = {
-    filterByProps: ['color', 'border-color']
+    filterByProps: ['color', 'border-color'],
 };
 
-test('default settings', t => {
-    let input = `.foo {
+test('default settings', (t) => {
+    const input = `.foo {
         color: #000;
         width: 10px;
         display: block;
     }`;
-    let output = `:root {
+    const output = `:root {
         --color-1: #000;
         --width-1: 10px;
         --display-1: block;\n}\n.foo {
@@ -32,14 +32,14 @@ test('default settings', t => {
     return run(t, input, output, { });
 });
 
-test('repeated values', t => {
-    let input = `.foo {
+test('repeated values', (t) => {
+    const input = `.foo {
         color: blue;
     }
     .bar {
         color: blue;
     }`;
-    let output = `:root {
+    const output = `:root {
         --color-1: blue;\n}
     .foo {
         color: var(--color-1);
@@ -51,77 +51,77 @@ test('repeated values', t => {
 });
 
 /** *************Color types*********************** **/
-test('HSL', t => {
-    let input = `.foo {
+test('HSL', (t) => {
+    const input = `.foo {
         border: 1px solid hsl(120, 100%, 50%);
     }`;
-    let output = `:root {
+    const output = `:root {
         --border-1: hsl(120, 100%, 50%);\n}\n.foo {
         border: 1px solid var(--border-1);
     }`;
     return run(t, input, output, { onlyColor: true });
 });
 
-test('HSLA', t => {
-    let input = `.foo {
+test('HSLA', (t) => {
+    const input = `.foo {
         border: 1px solid hsla(120, 100%, 50%, 0.5);
     }`;
-    let output = `:root {
+    const output = `:root {
         --border-1: hsla(120, 100%, 50%, 0.5);\n}\n.foo {
         border: 1px solid var(--border-1);
     }`;
     return run(t, input, output, { onlyColor: true });
 });
 
-test('color keyword', t => {
-    let input = `.foo {
+test('color keyword', (t) => {
+    const input = `.foo {
         border: 1px solid black;
     }`;
-    let output = `:root {
+    const output = `:root {
         --border-1: black;\n}\n.foo {
         border: 1px solid var(--border-1);
     }`;
     return run(t, input, output, { onlyColor: true });
 });
 
-test('RGB', t => {
-    let input = `.foo {
+test('RGB', (t) => {
+    const input = `.foo {
         border: 1px solid rgb(120, 100, 50);
     }`;
-    let output = `:root {
+    const output = `:root {
         --border-1: rgb(120, 100, 50);\n}\n.foo {
         border: 1px solid var(--border-1);
     }`;
     return run(t, input, output, { onlyColor: true });
 });
 
-test('RGBA', t => {
-    let input = `.foo {
+test('RGBA', (t) => {
+    const input = `.foo {
         border: 1px solid rgba(120, 100, 50, 0.4);
     }`;
-    let output = `:root {
+    const output = `:root {
         --border-1: rgba(120, 100, 50, 0.4);\n}\n.foo {
         border: 1px solid var(--border-1);
     }`;
     return run(t, input, output, { onlyColor: true });
 });
 
-test('short hex', t => {
-    let input = `.foo {
+test('short hex', (t) => {
+    const input = `.foo {
         border: 1px solid #000;
     }`;
-    let output = `:root {
+    const output = `:root {
         --border-1: #000;\n}\n.foo {
         border: 1px solid var(--border-1);
     }`;
     return run(t, input, output, { onlyColor: true });
 });
 
-test('long hex', t => {
-    let input = `.foo {
+test('long hex', (t) => {
+    const input = `.foo {
         border: 1px solid #101113;
     }`;
-    let output = `:root {
+    const output = `:root {
         --border-1: #101113;\n}\n.foo {
         border: 1px solid var(--border-1);
     }`;
@@ -130,15 +130,15 @@ test('long hex', t => {
 
 /** ************************************************** **/
 
-test('exist root element', t => {
-    let input = `:root {
+test('exist root element', (t) => {
+    const input = `:root {
         --base-font-size: 16px;
     }
     .foo {
         color: #000;
         font-size: var(--base-font-size);
     }`;
-    let output = `:root {
+    const output = `:root {
         --base-font-size: 16px;
         --color-1: #000;
     }
@@ -149,11 +149,11 @@ test('exist root element', t => {
     return run(t, input, output, { });
 });
 
-test('several colors in one property', t => {
-    let input = `.foo {
+test('several colors in one property', (t) => {
+    const input = `.foo {
         box-shadow: inset 0 2px 0px #dcffa6, 0 2px 5px #000;
     }`;
-    let output = `:root {
+    const output = `:root {
         --box-shadow-1: #dcffa6;
         --box-shadow-2: #000;\n}\n.foo {
         box-shadow: inset 0 2px 0px var(--box-shadow-1), 0 2px 5px var(--box-shadow-2);
@@ -161,14 +161,14 @@ test('several colors in one property', t => {
     return run(t, input, output, { onlyColor: true });
 });
 
-test('custom element for css variables', t => {
-    let input = `.foo {
+test('custom element for css variables', (t) => {
+    const input = `.foo {
         --color-1: black;
     }
     .bar {
         border-color: red;
     }`;
-    let output = `.foo {
+    const output = `.foo {
         --color-1: black;
         --border-color-1: red;
     }
@@ -178,31 +178,31 @@ test('custom element for css variables', t => {
     return run(t, input, output, { scope: '.foo' });
 });
 
-test('filter by color and props', t => {
-    let input = `.foo {
+test('filter by color and props', (t) => {
+    const input = `.foo {
         border: 1px solid #000;
         background-color: red;
     }`;
-    let output = `:root {
+    const output = `:root {
         --border-1: #000;\n}\n.foo {
         border: 1px solid var(--border-1);
         background-color: red;
     }`;
     return run(t, input, output, {
         onlyColor: true,
-        filterByProps: ['border']
+        filterByProps: ['border'],
     });
 });
 
-test('default value in css variable', t => {
-    let input = `:root {
+test('default value in css variable', (t) => {
+    const input = `:root {
         --base-color: #fff;
     }
     .foo {
         color: var(--base-color, #000);
         border: 1px solid #eee;
     }`;
-    let output = `:root {
+    const output = `:root {
         --base-color: #fff;
         --border-1: #eee;
     }
@@ -213,15 +213,15 @@ test('default value in css variable', t => {
     return run(t, input, output, { onlyColor: true });
 });
 
-test('variable with several color values', t => {
-    let input = `:root {
+test('variable with several color values', (t) => {
+    const input = `:root {
         --base-color: #fff;
     }
     .foo {
         border: 1px solid var(--base-color),
         2px solid #000;
     }`;
-    let output = `:root {
+    const output = `:root {
         --base-color: #fff;
         --border-1: #000;
     }
@@ -232,23 +232,23 @@ test('variable with several color values', t => {
     return run(t, input, output, { onlyColor: true });
 });
 
-test('variable template', t => {
-    let input = `.foo {
+test('variable template', (t) => {
+    const input = `.foo {
         border: 2px solid #000;
     }`;
-    let output = `:root {
+    const output = `:root {
         --theme-black-1: #000;\n}\n.foo {
         border: 2px solid var(--theme-black-1);
     }`;
     return run(t, input, output, { onlyColor: true, templateVariableName: 'theme[colorKeyword]' });
 });
 
-test('variable template with two black colors', t => {
-    let input = `.foo {
+test('variable template with two black colors', (t) => {
+    const input = `.foo {
         border: 2px solid #000;
         color: #020202;
     }`;
-    let output = `:root {
+    const output = `:root {
         --theme-black-1: #000;
         --theme-black-2: #020202;\n}\n.foo {
         border: 2px solid var(--theme-black-1);
@@ -257,13 +257,13 @@ test('variable template with two black colors', t => {
     return run(t, input, output, { onlyColor: true, templateVariableName: 'theme[colorKeyword]' });
 });
 
-test('variable template with light and dark color', t => {
-    let input = `.foo {
+test('variable template with light and dark color', (t) => {
+    const input = `.foo {
         border: 2px solid #cc0000;
         color: #ff0000;
         background-color: rgb(255, 26, 26);
     }`;
-    let output = `:root {
+    const output = `:root {
         --theme-dark-red-1: #cc0000;
         --theme-red-1: #ff0000;
         --theme-light-red-1: rgb(255, 26, 26);\n}\n.foo {
@@ -274,13 +274,13 @@ test('variable template with light and dark color', t => {
     return run(t, input, output, { onlyColor: true, templateVariableName: 'theme[tint][colorKeyword]' });
 });
 
-test('variable template with tint', t => {
-    let input = `.foo {
+test('variable template with tint', (t) => {
+    const input = `.foo {
         border: 2px solid #000;
         color: #020202;
         background-color: #0d0d0d;
     }`;
-    let output = `:root {
+    const output = `:root {
         --black-1: #000;
         --black-light-1: #020202;
         --black-light-2: #0d0d0d;\n}\n.foo {
@@ -291,13 +291,13 @@ test('variable template with tint', t => {
     return run(t, input, output, { onlyColor: true, templateVariableName: '[colorKeyword][tint]' });
 });
 
-test('variable template with tint in the beginning', t => {
-    let input = `.foo {
+test('variable template with tint in the beginning', (t) => {
+    const input = `.foo {
         border: 2px solid #000;
         color: #020202;
         background-color: #0d0d0d;
     }`;
-    let output = `:root {
+    const output = `:root {
         --black-1: #000;
         --light-black-1: #020202;
         --light-black-2: #0d0d0d;\n}\n.foo {
@@ -308,18 +308,18 @@ test('variable template with tint in the beginning', t => {
     return run(t, input, output, { onlyColor: true, templateVariableName: '[tint][colorKeyword]' });
 });
 
-test('template with propertyName', t => {
-    let input = `.foo {
+test('template with propertyName', (t) => {
+    const input = `.foo {
         border: 1px solid #000;
         background-color: red;
     }`;
-    let output = `:root {
+    const output = `:root {
         --theme-border-1: 1px solid #000;
         --theme-background-color-1: red;\n}\n.foo {
         border: var(--theme-border-1);
         background-color: var(--theme-background-color-1);
     }`;
     return run(t, input, output, {
-        templateVariableName: 'theme-[propertyName]'
+        templateVariableName: 'theme-[propertyName]',
     });
 });
