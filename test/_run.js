df796e54587095ff34ada2e10a8cab344b5834c9
lutien <lutien02@gmail.com>
2016-11-12 01:52:57 +0300

    Fixes for Node 4

--- test/_run.js
+++ test/_run.js
@@ -1,8 +1,9 @@
 const postcss = require('postcss');
 const plugin = require('../lib/index');
 
-module.exports = function run(t, input, output, opts = { }) {
-    return postcss([plugin(opts)]).process(input)
+module.exports = function run(t, input, output, opts) {
+    const params = opts || {}; // Fix for Node 4
+    return postcss([plugin(params)]).process(input)
         .then((result) => {
             t.deepEqual(result.css, output);
             t.deepEqual(result.warnings().length, 0);
